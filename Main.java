3df7790bb12216e15e53511006eaadc2e9ebf88a
Caston Boyd <cboyd@ebrooke.org>
2020-12-11 20:36:18 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.awt.*;
+
+public class TurtleDrawShapes
+{
+  public static void main(String[] args)
+  {
+      World world = new World(400,400);
+      Turtle yertle = new Turtle(world);
+      yertle.penUp();  // move a little to the left
+      yertle.moveTo(100,200);
+      yertle.penDown();
+      yertle.setColor(Color.blue);
+
+      // Add your loop here!
+      yertle.forward(100);
+      yertle.turn(90);
+
+      world.show(true);
+  }
+}
